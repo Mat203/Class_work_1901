0b1cdee26aa9360b20eb346753dcb49497c55192
Mat_Tol <matvey.talalaevskiy@gmail.com>
2024-01-23 16:17:19 +0200

    lecture 1

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/homeRoutes');
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'pug');
+app.set('views', 'vieІws');
 
 app.use('/', routes);
 const PORT = 3400;
 app.listen(PORT, () => {
-    console.log('Server i srunning on http://localhost:${PORT}');
+    console.log('Server is vrunning on http://localhost:${PORT}');
 });
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -5,4 +5,8 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 router.get('/',homeController.getHomePage);
 
+router.get('/pug', (req,res) => {
+res.render('index', {title:'Hey', message:'Hello there!'})
+})
+
 module.exports = router;
